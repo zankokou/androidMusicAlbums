be12f5947200f58cff13954af7922630fc0f15ee
Ken <khuynh024@gmail.com>
2018-10-20 17:52:28 -0500

    updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # androidMusicAlbums
 Android application where the user can view songs form various artists
+Made with react-native 
+
+#Dependencies
+* Android Studio
+
+#How to Run
+1. clone repo
+2. npm install
+3. react-native run-android
---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text, View } from 'react-native'
+
+// Create a component
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+
+    );
+
+};
+  
+
+const styles = {
+    viewStyle:{
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.2,
+        elevation: 5,
+        position: 'relative'
+    },
+    
+    textStyle: {
+        fontSize: 20,
+        // marginLeft: 20
+    }
+
+
+};
+
+// Make the component available to other parts of the app
+
+export default Header;
